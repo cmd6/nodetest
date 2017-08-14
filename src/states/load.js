b014c0779a023b19df7943bca968064600ebc7fa
Chris Davis <github@vu6.me>
2017-08-14 01:09:01 -0500

    Fixed so it would work locally; honestly not sure why the node http-server version even worked

--- src/states/load.js
+++ src/states/load.js
@@ -7,7 +7,7 @@ export class Load extends Phaser.State{
 		let textStyle = {font: '45px Arial', alight: 'center', stroke: 'blue', fill: 'blue'};
 
 		this.game.add.text(80,150,'loading...',textStyle);
-		this.game.load.spritesheet('wizard','../assets/wizardsprite.png',95,123,6);
+		this.game.load.spritesheet('wizard','./assets/wizardsprite.png',95,123,6);
 	}
 
 	create(){
