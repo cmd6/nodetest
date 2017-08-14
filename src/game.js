a057f8c7536329797f718d397528282b57b84737
cmd6 <github@vu6.me>
2017-08-13 23:28:24 -0500

    basic shit

---
+++ src/game.js
@@ -0,0 +1,24 @@
+// src/game.js
+
+import Phaser from 'phaser';
+import { Load } from './states/load';
+import { Menu } from './states/menu';
+import { Play } from './states/play';
+import { GameOver } from './states/gameover';
+import { Victory } from './states/victory';
+
+export class Game extends Phaser.Game{
+	constructor(){
+		super(800,600,Phaser.AUTO,'phaser-canvas',{create: () => {
+			this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+			this.state.add('load',Load);
+			this.state.add('menu', Menu);
+            this.state.add('play', Play);
+            this.state.add('gameover', GameOver);
+			this.state.add('victory', Victory);
+			
+			this.state.start('load');
+		}});
+	}
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+// src/main.js
+// i might be out of my depth: http://www.thecodingcouple.com/getting-started-phaser-es2015/
+import{Game} from './game';
+
+new Game();
